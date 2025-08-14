d89892812ea5255a21c746d0c16e0e397c5dd530
Jeremy Sauer <jsauer@ucar.edu>
2025-08-14 14:33:41 -0600

    Made a pass at optimizations of the python converter script owing to the change in NCAR HPC environment policy making use of swap space no longer permissible. The script now requires much less memory, essentially twice the total dataset size per process to convert from raw per-rank binary files into a single netcdf file. It is always significantly more performant under adoption of np.frombuffer instead of using explicit struct.unpack for parsing binary array data.

--- scripts/python_utilities/post-processing/FEbinaryToNetCDF.py
+++ scripts/python_utilities/post-processing/FEbinaryToNetCDF.py
@@ -14,69 +14,157 @@ from mpi4py import MPI
 def field3dTranspose(fld,extents):
     fld=fld.reshape(extents)
     fldFinal=np.transpose(fld,axes=[2,1,0])
+    del fld
     return fldFinal[np.newaxis,Nh:-Nh,Nh:-Nh,Nh:-Nh]
 
 def field2dTranspose(fld,extents):
     fld=fld.reshape(extents)
     fldFinal=np.transpose(fld,axes=[1,0])
+    del fld
     return fldFinal[np.newaxis,Nh:-Nh,Nh:-Nh]
 
-def readBinary(outpath,theseFiles):
-    verboseLogging=False
+def readBinary(numOutRanks,outpath,theseFiles):
+    
+    if 'fulldata_dict' in locals():
+        del fulldata_dict
+    if 'subdata_dict' in locals():
+        del subdata_dict
+    gc.collect()
+
+    verboseLogging=False #True 
     print(theseFiles)
-    dsSet=[]
+    fulldata_dict = {}
+
     for thatFile in theseFiles:
+       subdata_dict = {}
        print(thatFile)
-       ds_fe=xr.Dataset()
        try:
           thisFile='{:s}{:s}'.format(outpath,thatFile)
           flength = os.stat(thisFile).st_size
           with open(thisFile, mode='rb') as f:
-               while(f.tell() < flength):
+               while(f.tell() < flength): #while the filepointer is not at the end of the binary file
+                 ## Read and parse the binary representation of 
+                 ## the current variable "name" (a string of integer length)
                  nameLen=struct.unpack("i", f.read(4))
                  if verboseLogging:
                      print(len(nameLen),nameLen[0])
                  fieldName=f.read(nameLen[0]).rstrip(b'\x00').decode()
                  if verboseLogging:
                      print(fieldName)
+                 ## Read and parse the binary representation of 
+                 ## the current variable "type" (a string of integer length)
                  typeLen=struct.unpack("i", f.read(4))
                  if verboseLogging:
                      print(len(typeLen),typeLen[0])
                  fieldType=f.read(typeLen[0]).rstrip(b'\x00').decode()
+                 ## Read and parse the binary representation of 
+                 ## the current variable "number-of-dimensions" (integer) 
                  nDims=struct.unpack("i", f.read(4))
                  if verboseLogging:
                      print(nDims)
+                 ## Read and parse the binary representation of 
+                 ## the current variable "dimension extents" (1-d integer array) 
                  extents=np.array([],dtype=np.int32)
                  fmtStr='{:d}i'.format(nDims[0])
                  extents=np.asarray(struct.unpack(fmtStr,f.read(nDims[0]*4)),dtype=np.int32)
                  if verboseLogging:
                      print(extents)
+                 ## Read, parse, reshape, transpose the binary field based on the type and extents  
                  if fieldType == 'float':
-                    fmtStr='{:d}f'.format(np.prod(extents))
-                    fld=np.asarray(struct.unpack(fmtStr,f.read(np.prod(extents)*4)),dtype=np.float32)
+                     fmtStr='{:d}f'.format(np.prod(extents))
+                     if(len(extents)==3):
+                         fld3dfloat=np.frombuffer(f.read(np.prod(extents)*4),dtype=np.float32).reshape(extents)
+                         fldFinal=field3dTranspose(fld3dfloat,extents)
+                         if verboseLogging:
+                             print(fldFinal.shape)
+                     elif(len(extents)==2):
+                         fld2dfloat=np.frombuffer(f.read(np.prod(extents)*4),dtype=np.float32).reshape(extents)
+                         fldFinal=field2dTranspose(fld2dfloat,extents)
+                         if verboseLogging:
+                             print(fldFinal.shape)
+                     elif(len(extents)==1):
+                         fld1dfloat=np.frombuffer(f.read(np.prod(extents)*4),dtype=np.float32)
+                         fldFinal=fld1dfloat
+                         if verboseLogging:
+                             print(fldFinal.shape)
                  elif fieldType == 'int':
-                    fmtStr='{:d}i'.format(np.prod(extents))
-                    fld=np.asarray(struct.unpack(fmtStr,f.read(np.prod(extents)*4)),dtype=np.int32)
-                 if(len(extents)==3):
-                     fld=fld.reshape(extents)
-                     fldFinal=field3dTranspose(fld,extents)
-                     if verboseLogging:
-                         print(fldFinal.shape)
-                     ds_fe[fieldName]=xr.DataArray(fldFinal,dims=['time','zIndex','yIndex','xIndex'])
-                 elif(len(extents)==2):
-                     fldFinal=field2dTranspose(fld,extents)
-                     if verboseLogging:
-                         print(fldFinal.shape)
-                     ds_fe[fieldName]=xr.DataArray(fldFinal,dims=['time','yIndex','xIndex'])
-                 elif(len(extents)==1):
-                     fldFinal=fld
-                     ds_fe[fieldName]=xr.DataArray(fldFinal,dims=['time'])
+                     fmtStr='{:d}i'.format(np.prod(extents))
+                     if(len(extents)==1):
+                         fld1dint=np.frombuffer(f.read(np.prod(extents)*4),dtype=np.int32)
+                         fldFinal=fld1dint
+                         if verboseLogging:
+                             print(fldFinal.shape)
+                 ### Add the named field to a dictionary as a key-value pair 
+                 subdata_dict[fieldName]=fldFinal
        except IOError:
          print('Error While Opening the file: {:s}'.format(thisFile))
-       dsSet.append(ds_fe)
-      
-    #Concatenate all the perRank dataSets into a single dataset 
-    dsFull=xr.concat(dsSet,'xIndex',data_vars='minimal')
+       finally:
+        if f:  # Check if f was successfully assigned a file object
+            f.close()
+            # The file is closed here
+       ### If this is the first time through allocate all the full data arrays 
+       ### necessary for a full data dictionary
+       if len(fulldata_dict) == 0:
+           rank_cnt=0
+           for key in subdata_dict.keys():
+               subextents = subdata_dict[key].shape
+               fullextents = subextents
+               if len(subextents) > 1:  ## Note tuples are immutable so using tuple(list(blah_tuple)) as workaround 
+                                        ## to compute xIndex value in fullextents 
+                   list_extents = list(subextents)
+                   list_extents[-1] = list_extents[-1]*numOutRanks
+                   fullextents = tuple(list_extents)
+               if verboseLogging:
+                   print(f"rank_cnt = {rank_cnt}, {key}: subextents = {subextents}, fullextents={fullextents}")
+               subtype = subdata_dict[key].dtype
+               fulldata_dict[key] = np.zeros(fullextents,dtype=subtype)
+               if verboseLogging:
+                    print(f"rank_cnt = {rank_cnt}, {key}:  fulldata_dict[key].shape= {fulldata_dict[key].shape}, fullextents={fullextents}")
+               ## NOTE for now this assumes concatenation is always on the xIndex 
+               ## (just like the original converter script)!!!
+               if len(subextents) == 1:    #[time]
+                   fulldata_dict[key] = np.copy(subdata_dict[key])
+               elif len(subextents) == 3:  #[time, yIndex, xIndex]
+                   fulldata_dict[key][:,:,rank_cnt*subextents[-1]:(rank_cnt+1)*subextents[-1]] = np.copy(subdata_dict[key])
+               elif len(subextents) == 4:  #[time, zIndex, yIndex, xIndex]
+                   if verboseLogging:
+                       print(f"rank_cnt = {rank_cnt}, {key}:  slice.shape= {fulldata_dict[key][:,:,:,rank_cnt*subextents[-1]:(rank_cnt+1)*subextents[-1]].shape}, fullextents={fullextents}")
+                   fulldata_dict[key][:,:,:,rank_cnt*subextents[-1]:(rank_cnt+1)*subextents[-1]] = np.copy(subdata_dict[key])
+           rank_cnt += 1
+       else:  ## This is the second or later file in the per-rank sequence so just fill fullData array segments
+           for key in subdata_dict.keys():
+               subextents = subdata_dict[key].shape
+               fullextents = fulldata_dict[key].shape
+               if verboseLogging:
+                   print(f"rank_cnt = {rank_cnt}, {key}: subextents = {subextents}, fullextents={fullextents}")
+               subtype = subdata_dict[key].dtype
+               if len(subextents) == 1: 
+                   if verboseLogging:
+                      print(f"rank_cnt = {rank_cnt}, skipping {key} since no spatial dimensions involved...")
+               elif len(subextents) == 3:
+                   fulldata_dict[key][:,:,rank_cnt*subextents[-1]:(rank_cnt+1)*subextents[-1]] = np.copy(subdata_dict[key])
+               elif len(subextents) == 4:
+                   fulldata_dict[key][:,:,:,rank_cnt*subextents[-1]:(rank_cnt+1)*subextents[-1]] = np.copy(subdata_dict[key])
+           rank_cnt += 1
+              
+    #Create the full-domain single xarray dataset
+    dsFull=xr.Dataset()
+    for key in fulldata_dict.keys():
+        if verboseLogging:
+             print(f"rank_cnt = {rank_cnt}, creating dataset field {key} with shape {fulldata_dict[key].shape}...")
+        if len(fulldata_dict[key].shape) == 4:
+            dsFull[key]=xr.DataArray(fulldata_dict[key],dims=['time','zIndex','yIndex','xIndex'])
+        if len(fulldata_dict[key].shape) == 3:
+            dsFull[key]=xr.DataArray(fulldata_dict[key],dims=['time','yIndex','xIndex'])
+        if len(fulldata_dict[key].shape) == 1:
+            dsFull[key]=xr.DataArray(fulldata_dict[key],dims=['time'])
+   
+    ## Clean up memory 
+    del subdata_dict
+    del fulldata_dict
+    del fldFinal 
+    gc.collect()
+
     return dsFull
 
 ###
@@ -89,7 +177,7 @@ def parse_args():
     args = parser.parse_args()
     return args
 
-################## main()
+################## main() ################################################################################
 print("Hello performing first MPI calls.")
 
 mpi_size = MPI.COMM_WORLD.Get_size()
@@ -120,7 +208,7 @@ if mpi_size <= fileSetSize:
 else:
   print('mpi_size of {:d} is > fileSetSize = {:d}. Please ensure mpi_size <= fileSetSize.*'.format(mpi_size,fileSetSize))
   exit()
-tstop=tstart+tstep*fileSetSize   #tstart+(mpi_size*fileBatchsize+1)*tstep
+tstop=tstart+tstep*fileSetSize   
 print("{:d}/{:d}: Hello World! on {:s}.".format(mpi_rank, mpi_size, mpi_name))
 print('Converting binary FE outputs {:s}{:s}_rank_{:d}-{:d}.*'.format(outpath,FEoutBase,0,numOutRanks))
 print('In batches of {:d} files per rank beginning from timestep {:d} to timestep {:d} every {:d} timesteps.'.format(fileBatchsize,tstart,tstop,tstep))
@@ -172,19 +260,19 @@ for timeStep in mytslist:
    else:
        print('{:d} specified binary files are missing. Skipping timestep: {:d}...'.format(numOutRanks-goodCnt,timeStep))
    if parseProceed:
-     dsFull=readBinary(outpath,theseFiles)
+     dsFull=readBinary(numOutRanks,outpath,theseFiles)
      #write the full  domain datatset to netcdf file
      if False:
         dsFull.to_netcdf('{:s}NETCDF/{:s}.{:d}'.format(outpath,FEoutBase,timeStep),format='NETCDF4')
      else:
         dsFull.to_netcdf('{:s}/{:s}.{:d}'.format(netCDFpath,FEoutBase,timeStep),format='NETCDF4')
      del dsFull
+     gc.collect()
      if os.path.exists('{:s}/{:s}.{:d}'.format(netCDFpath,FEoutBase,timeStep)):
        if removeBinaries:
          for thatFile in theseFiles:
            thisFile='{:s}{:s}'.format(outpath,thatFile)
            os.remove(thisFile)
-   gc.collect()
 
 MPI.COMM_WORLD.Barrier()
 print("{:d}/{:d}: Conversions complete on {:s}.".format(mpi_rank, mpi_size, mpi_name))
